9c97ffcb406a1ac4d7e6eed703cb557e0e84336e
hchekuri <42903521+hchekuri@users.noreply.github.com>
2020-01-21 18:13:43 -0500

    Add files via upload

---
+++ Subject.java
@@ -0,0 +1,13 @@
+package com.st.dht;
+
+/**
+ * Created by tarun on 13-02-2018.
+ */
+
+public class Subject {
+
+    public String Subject_Name;
+    public String Subject_Full_Form;
+    public String date_time;
+
+}
